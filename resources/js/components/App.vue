d0bb938a8161bddabf538cc68e744d81c0628a14
Oğuzhan Yılmaz <45510538+1oguzhanyilmaz@users.noreply.github.com>
2020-06-12 19:12:50 +0300

    tailwindcss setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="border-solid border-4 border-gray-600">App Component</div>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
